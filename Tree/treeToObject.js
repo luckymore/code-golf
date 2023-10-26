16a73386801beb1ecff46dad8de81bf314053598
张宁宁 <zhangnn1@yonghui.cn>
2023-10-26 14:05:08 +0800

    :sparkles: 深层对象与简单对象的转化

--- Tree/treeToObject.js
+++ Tree/treeToObject.js
@@ -19,20 +19,46 @@ var output = {
   'a.e': 'ae'
 }
 
+
 // entry 转成 output
-const transformObject = (obj, prefix = '') => {
+const fromTree = (tree, prefix = '') => {
   let result = {};
 
-  for (let key in obj) {
-    if (typeof obj[key] === 'object') {
-      let nestedKeys = transformObject(obj[key], `${prefix}${key}.`);
+  for (let key in tree) {
+    if (typeof tree[key] === 'object') {
+      let nestedKeys = fromTree(tree[key], `${prefix}${key}.`);
       result = { ...result, ...nestedKeys };
     } else {
-      result[`${prefix}${key}`] = obj[key];
+      result[`${prefix}${key}`] = tree[key];
     }
   }
 
   return result;
 };
 
-console.log('transformObject(entry):', transformObject(entry))
+// output 转 entry
+const fromObj = (output) => {
+  return Object.entries(output).reduce((acc, [key, value]) => {
+    const keys = key.split('.');
+    let currentObj = acc
+
+    for (let i = 0; i < keys.length; i++) {
+      const currentKey = keys[i];
+
+      if (i === keys.length - 1) {
+        currentObj[currentKey] = value;
+      } else {
+        if (!currentObj[currentKey]) {
+          currentObj[currentKey] = {};
+        }
+        currentObj = currentObj[currentKey];
+      }
+    }
+
+    return acc
+  }, {})
+}
+
+console.log('fromTree(entry):', fromTree(entry))
+const outputRes = fromObj(output)
+console.log('fromObj(output):', outputRes)
