be538221896d63c593dfbfb0170191475197fc8a
张宁宁 <zhangnn1@yonghui.cn>
2023-11-10 10:54:09 +0800

    :sparkles: 深拷贝,支持 Set/Map/Map内的循环引用

--- deepClone.js
+++ deepClone.js
@@ -2,58 +2,64 @@
  * 深拷贝，考虑循环引用问题
  * 需要实现 Set/Map/Symbol 的拷贝
  */
-const deepClone = (target, map = new WeakMap()) => {
+const deepClone = (target, hashMap = new WeakMap()) => {
   // Check if the target is a primitive data type
-  if (Object(target) !== target || typeof target === 'function') {
+  if (target === null || typeof target !== 'object') {
     return target
   }
 
+  if (/^[Function|RegExp|Date|Error]+$/.test(target.constructor.name)) return new target.constructor(target)
+
   // Check if the target has already been cloned
-  if (map.has(target)) {
-    return map.get(target)
+  if (hashMap.has(target)) {
+    return hashMap.get(target)
   }
 
-  // Create a new empty object or array to store the cloned properties
-  let clone = Array.isArray(target) ? [] : {}
-
-  // Store the clone in the map
-  map.set(target, clone)
+  let clone
 
   // Clone the properties of the target
   if (target instanceof Set) {
-    const set = clone = new Set()
+    clone = new Set()
     target.forEach(value => {
-      set.add(deepClone(value, map))
+      clone.add(deepClone(value, hashMap))
     })
   } else if (target instanceof Map) {
-    const map = new Map()
+    clone = new Map()
     target.forEach((value, key) => {
       if (Object(value) !== value) {
-        map.set(key, value)
+        clone.set(key, value)
       } else {
-        map.set(key, deepClone(value, map))
+        clone.set(key, deepClone(value, hashMap))
       }
     })
-    clone = map
-  } else if (target instanceof Symbol) {
-    clone = Object(Symbol.prototype.valueOf.call(target))
-  } else if (target instanceof Date) {
-    clone = new Date(target)
   } else {
-    Object.keys(target).forEach(key => {
-      clone[key] = deepClone(target[key], map)
-    })
+    clone = Array.isArray(target) ? [] : {}
+    hashMap.set(target, clone)
+    for (let key in target) {
+      if (target.hasOwnProperty(key)) {
+        clone[key] = deepClone(target[key], hashMap)
+      }
+    }
   }
 
   return clone
 }
 const nest = { flag: false }
-const circleObj = { a: 1, date: new Date('2020'), ss: Symbol(1), set: new Set([1, 2]), map: new Map([[1, nest]]) }
+const mapCircle = new Map([[1, 2]])
+const circleObj = {
+  a: 1,
+  nest,
+  date: new Date('2020'),
+  ss: Symbol(1), set: new Set([1, 2]), map: mapCircle
+}
 circleObj.c = circleObj
+mapCircle.set(3, circleObj)
 
 const clone = deepClone(circleObj)
 nest.flag = true
 circleObj.set.add(3)
+mapCircle.set(4, 4)
+circleObj.xxx = 'xxx'
 console.log(clone)
 console.log('symbol', clone.ss === circleObj.ss)
 console.log('date', clone.date === circleObj.date, clone.date)
